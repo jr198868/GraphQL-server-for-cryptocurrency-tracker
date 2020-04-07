397390cb1dcd8c6a7f67dd9f5966261337264207
jr198868 <rjingraymond88@gmail.com>
2020-04-07 02:12:37 -0400

    Add files via upload

---
+++ graphqlcrypto/graphqlcrypto/schema.py
@@ -0,0 +1,12 @@
+import graphene
+
+import data.schema
+
+
+class Query(data.schema.Query, graphene.ObjectType):
+    pass
+
+class Mutation(data.schema.Mutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
